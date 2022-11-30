e554398a0349b8ab1de83cdf85ae7acd386ec00e
Asandile <asandilelangeni@gmail.com>
2022-11-30 13:54:07 +0200

    second commit

--- js/code.js
+++ js/code.js
@@ -50,8 +50,8 @@
 // }
 
 //Nested if statement (not liked by Joel)
-let age = 18;
-let salary = 4000;
+// let age = 18;
+// let salary = 4000;
 // if(age > 17) {
 //     if(salary >= 5000); {
 //         console.log("well done");
@@ -75,22 +75,72 @@ let salary = 4000;
 //         break;
 // }
 
-let grade = 48;
-switch(true) {
-    case grade == 100:
-        console.log("You're awesome")
-    break;
-    case (grade >= 90) && (grade <= 99):
-        console.log("well done");
-    break;
-    case (grade >= 75) && (grade <= 89):
-        console.log("distinction");
-    break;
-    case (grade >= 50) && (grade <= 74):
-        console.log("pass");
-     case (grade <= 49):
-        console.log("You're in a dog");
-    break;
-    default:
-        console.log("Out of range")
+// let grade = 48;
+// switch(true) {
+//     case grade == 100:
+//         console.log("You're awesome")
+//     break;
+//     case (grade >= 90) && (grade <= 99):
+//         console.log("well done");
+//     break;
+//     case (grade >= 75) && (grade <= 89):
+//         console.log("distinction");
+//     break;
+//     case (grade >= 50) && (grade <= 74):
+//         console.log("pass");
+//      case (grade <= 49):
+//         console.log("You're in a dog");
+//     break;
+//     default:
+//         console.log("Out of range")
+// }
+
+// for in loop
+let numbers = [8, 9, 3, 12, 34];
+// for(let numb in numbers) {
+//     console.log(numb);
+// }
+// for(let numb in numbers) {
+//   console.table(numbers)
+// }
+
+// let cnt = 0;
+// while(cnt < numbers.length) {
+//     console.log(`step: ${numbers[cnt]}`);
+//     cnt++;
+// }
+//do ... while
+// do{
+//     console.log(`step: ${numbers[cnt]}`);
+//     cnt++;
+// }while(cnt < numbers.length);
+
+
+// function
+// function addition(numb1, numb2) {
+//     console.log("Sum is:", numb1 + numb2);
+// }
+// addition(4, 5);
+// addition(5, 7);
+// addition(9, 23);
+
+// ternary operator
+// let age = 17;
+// console.log(age >17 ? "You're qualified": "you in a dog");
+
+//Rest Operator
+// function sumOf(...arg) {
+//     return arg.reduce((a, b)=>{
+//         return a + b
+//     })
+// }
+// console.log(sumOf(1, 2, 3))
+
+//Constructor function
+function Person(...details) {
+    this.firstName = details[0];
+    this.lastName = details[1];
+    this.email = details.at(-1);
 }
+let person1 = new Person("Konke", "Deno", "deno@gmail.com")
+console.log(person1)
